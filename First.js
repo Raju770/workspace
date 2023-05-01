a5c3e9fafb88435296498aa11392bb8fe3bf5eac
Raju770 <131154521+Raju770@users.noreply.github.com>
2023-05-01 14:49:49 +0530

    Add files via upload

---
+++ First.js
@@ -0,0 +1,28 @@
+
+let laptop2 = {
+        cpu : 'i7',
+        ram : 16,
+        brand : 'apple',
+        
+        x: function(){
+            console.log(laptop2.cpu);
+        }
+    }
+    let laptop1 = {
+        cpu : 'i9',
+        ram : 16,
+        brand : 'apple',
+        
+        compare: function(other){
+            if(this.cpu > other.cpu) {
+                console.log(this);
+            }
+            else
+                console.log(other);
+        }, 
+        x: function(){
+            console.log(this.cpu);
+    
+        }
+    }
+    laptop1.compare(laptop2);
